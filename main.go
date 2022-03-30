89ffe4f3dbc3973a4bd5aed2952a8001aed2289d
junckycat <91248062+junckycat@users.noreply.github.com>
2022-03-30 10:28:58 +0200

    commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+)
+
+type User struct {
+	ID int64
+
+	Name string
+
+	Lastname string
+
+	Email string
+
+	Born string
+
+	Country string
+
+	City string
+
+	Postal string
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		tmpl := template.Must(template.ParseFiles("front/index.html"))
+		tmpl.Execute(w, nil)
+	})
+
+	root, _ := os.Getwd()
+	sf := http.FileServer(http.Dir(root + "/front"))
+	http.Handle("/static/", http.StripPrefix("/static/", sf))
+
+	http.ListenAndServe(":8000", nil)
+}
